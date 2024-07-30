93e10e448e3c402a78909355e011936fa4ed79d6
MoonAmon <misterofclans.mo@gmail.com>
2024-07-30 18:17:01 -0300

    javascript function

---
+++ javascript/first-steps/index.js
@@ -0,0 +1,23 @@
+function multiply(x, y) {
+    return x * y;
+}
+
+function yell(string_to_caps) {
+    console.log(string_to_caps.toUpperCase())
+}
+
+function longerThan(first_array, second_array) {
+    return first_array.length > second_array.length;
+}
+
+yell("hi sir, welcome!");
+
+const divide = (x,y) => x / y;
+
+const whisper = upper_string => {
+    return upper_string.toLowerCase();
+}
+
+const shorter_than = (first_array, second_array) => {
+    return first_array.length < second_array.length;
+}
